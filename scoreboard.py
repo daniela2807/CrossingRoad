13040ed28bc4580f60c182ef2f1f37a06f9506c7
DanielaGutierrez <dany_julio28@hotmail.com>
2023-01-23 15:12:52 -0600

    file records

--- scoreboard.py
+++ scoreboard.py
@@ -6,10 +6,12 @@ FONT = ("Courier", 14, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.level = 1
         super().__init__()
         self.penup()
-        self.high_score = 0
+        #self.high_score = 0
         self.hideturtle()
         self.sety(260)
         #self.write(f"Level = {self.level}", True, align="right",font = FONT)
@@ -21,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.high_score < self.level:
             self.high_score = self.level
+        with open("data.txt",mode="w") as file:
+            file.write(str(self.high_score))
         self.level=1
         self.updatescore()
 
